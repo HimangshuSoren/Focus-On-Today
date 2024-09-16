f4967e45ebfa6b59e8daac7d1746bfe9b083e120
Himangshu <67628306+HimangshuSoren@user.noreplay.github.com>
2024-09-16 08:05:34 +0530

    version-0.6

--- script.js
+++ script.js
@@ -2,17 +2,25 @@ const checkbox1 = document.querySelector('.coustom-checkbox-1')
 const check1 = document.querySelector('#green-check-1')
 checkbox1.addEventListener('click',()=>{
     check1.classList.toggle('vis')
+    checkbox1.classList.toggle('green-bg')
+    
 
 })
 const checkbox2 = document.querySelector('.coustom-checkbox-2')
 const check2 = document.querySelector('#green-check-2')
 checkbox2.addEventListener('click',()=>{
     check2.classList.toggle('vis')
+    checkbox2.classList.toggle('green-bg')
+    
+
 
 })
 const checkbox3 = document.querySelector('.coustom-checkbox-3')
 const check3 = document.querySelector('#green-check-3')
 checkbox3.addEventListener('click',()=>{
     check3.classList.toggle('vis')
+    checkbox3.classList.toggle('green-bg')
+    
+
 
 })
