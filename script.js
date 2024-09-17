6f694ed7b70f99271d06b5efadf7e3337f3a8e46
Himangshu <67628306+HimangshuSoren@user.noreplay.github.com>
2024-09-17 22:47:39 +0530

    javascript-code-revised

--- script.js
+++ script.js
@@ -1,27 +1,15 @@
-const checkbox1 = document.querySelector('.coustom-checkbox-1')
-const check1 = document.querySelector('#green-check-1')
-const goalText1 = document.querySelector('#goal-text-1')
-const goalText2 = document.querySelector('#goal-text-2')
-const goalText3 = document.querySelector('#goal-text-3')
-checkbox1.addEventListener('click',()=>{
-    check1.classList.toggle('vis')
-    checkbox1.classList.toggle('green-bg')
-    goalText1.classList.toggle('green-text')
+const checkIconList = document.querySelectorAll('.tick')
+const goalList = document.querySelectorAll('.goal-input')
 
-})
-const checkbox2 = document.querySelector('.coustom-checkbox-2')
-const check2 = document.querySelector('#green-check-2')
-checkbox2.addEventListener('click',()=>{
-    check2.classList.toggle('vis')
-    checkbox2.classList.toggle('green-bg')
-    goalText2.classList.toggle('green-text')
 
-})
-const checkbox3 = document.querySelector('.coustom-checkbox-3')
-const check3 = document.querySelector('#green-check-3')
-checkbox3.addEventListener('click',()=>{
-    check3.classList.toggle('vis')
-    checkbox3.classList.toggle('green-bg')
-    goalText3.classList.toggle('green-text')
+checkIconList.forEach((element)=>{
+        element.addEventListener('click',(event)=>{
+            event.target.parentElement.classList.toggle('green-bg')
+            event.target.parentElement.nextElementSibling.classList.toggle('complete')
 
+            // console.dir(event.target.parentElement.nextElementSibling)
+
+        })
 })
+
+
