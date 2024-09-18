af85420c9ba60806b88e044c14536f37a3695388
Himangshu <67628306+HimangshuSoren@user.noreplay.github.com>
2024-09-18 07:32:23 +0530

    Javascript-Improved

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
-const checkIconList = document.querySelectorAll('.tick')
-const goalList = document.querySelectorAll('.goal-input')
+const checkIconList = document.querySelectorAll('.tick') //---->[selecting all the checkboxes]
+const goalList = document.querySelectorAll('.goal-input')//---->[selecting all the input fields]
 
 
-checkIconList.forEach((element)=>{
-        element.addEventListener('click',(event)=>{
+checkIconList.forEach((element) => { //---->[function for all 3 checkboxes]
+    element.addEventListener('click', (event) => {
+        if ([...goalList].every((inputField) => {return inputField.value.trim()})) {
+            document.querySelector('.error-label').classList.add('vis')
             event.target.parentElement.classList.toggle('green-bg')
             event.target.parentElement.nextElementSibling.classList.toggle('complete')
-
-            // console.dir(event.target.parentElement.nextElementSibling)
-
-        })
+        }
+        else {
+            document.querySelector('.error-label').classList.remove('vis')
+        }
+        // console.dir(event.target.parentElement.nextElementSibling)
+    })
 })
 
 
